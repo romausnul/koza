642ad1c02f871702695d733c674f6feb975f042c
РомаН <119739400+romausnul@users.noreply.github.com>
2023-03-23 12:52:49 +0300

    Add files via upload

---
+++ КОЗЛИК В ОГОРОД/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace КОЗЛИК_В_ОГОРОД
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны огорода");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите длину веревки");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double s = Math.PI * r * r;
+            if (l < 2 * r)
+            {
+                double a = Math.Acos(l / 2 / r);
+                s = 4 * (a * r * r - l / 2 * r * Math.Sin(a));
+            }
+            Console.WriteLine($"Коза съела {s} кв.м");
+            Console.ReadKey();
+        }
+    }
+}
